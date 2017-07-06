a3c3c2560c0a18b47cd44c28da6c1cda1166f4f6
DaSch93 <d.schoenberger@ymail.com>
2017-07-06 14:54:50 +0200

    finish beim startscreen

--- app/src/main/java/com/example/danielojea/srapp/CharacterSelection.java
+++ app/src/main/java/com/example/danielojea/srapp/CharacterSelection.java
@@ -1,7 +1,9 @@
 package com.example.danielojea.srapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CharacterSelection extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class CharacterSelection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_selection);
+    }
+
+    public void startMetatype(View v){
+        Intent metaIntent = new Intent(this, Metatyp.class);
+        startActivity(metaIntent);
     }
 }
