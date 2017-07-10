1f0dac77c659cecb49aaf57c122739819a90dbd8
DaSch93 <d.schoenberger@ymail.com>
2017-07-10 14:53:13 +0200

    neues Seite mit Image

--- app/src/main/java/com/example/danielojea/srapp/Metatyp.java
+++ app/src/main/java/com/example/danielojea/srapp/Metatyp.java
@@ -1,116 +1,106 @@
 package com.example.danielojea.srapp;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 
 public class Metatyp extends AppCompatActivity {
 
+    String chosenMetatyp = "default";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metatyp);
     }
 
-    // Buttonimage wird beim klickevent gewechselt.
     public void changeElf(View v){
-        ImageButton elfButton = (ImageButton) findViewById(R.id.ElfButton);
-        ImageButton humanButton = (ImageButton) findViewById(R.id.HumanButton);
-        ImageButton dwarfButton = (ImageButton) findViewById(R.id.DwarfButton);
-        ImageButton orcButton = (ImageButton) findViewById(R.id.OrcButton);
-        ImageButton trollButton = (ImageButton) findViewById(R.id.TrollButton);
+        chosenMetatyp = "elf";
+        choseMetertyp("elf");
 
-        elfButton.setImageResource(R.drawable.metatyp_elf);
-        humanButton.setImageResource(R.drawable.metatyp_human_bw);
-        dwarfButton.setImageResource(R.drawable.metatyp_dwarf_bw);
-        orcButton.setImageResource(R.drawable.metatyp_orc_bw);
-        trollButton.setImageResource(R.drawable.metatyp_troll_bw);
     }
 
     public void changeHuman(View v){
-        ImageButton elfButton = (ImageButton) findViewById(R.id.ElfButton);
-        ImageButton humanButton = (ImageButton) findViewById(R.id.HumanButton);
-        ImageButton dwarfButton = (ImageButton) findViewById(R.id.DwarfButton);
-        ImageButton orcButton = (ImageButton) findViewById(R.id.OrcButton);
-        ImageButton trollButton = (ImageButton) findViewById(R.id.TrollButton);
-
-        elfButton.setImageResource(R.drawable.metatyp_elf_bw);
-        humanButton.setImageResource(R.drawable.metatyp_human);
-        dwarfButton.setImageResource(R.drawable.metatyp_dwarf_bw);
-        orcButton.setImageResource(R.drawable.metatyp_orc_bw);
-        trollButton.setImageResource(R.drawable.metatyp_troll_bw);
+        chosenMetatyp = "human";
+        choseMetertyp("human");
     }
 
     public void changeDwarf(View v){
-        ImageButton elfButton = (ImageButton) findViewById(R.id.ElfButton);
-        ImageButton humanButton = (ImageButton) findViewById(R.id.HumanButton);
-        ImageButton dwarfButton = (ImageButton) findViewById(R.id.DwarfButton);
-        ImageButton orcButton = (ImageButton) findViewById(R.id.OrcButton);
-        ImageButton trollButton = (ImageButton) findViewById(R.id.TrollButton);
-
-        elfButton.setImageResource(R.drawable.metatyp_elf_bw);
-        humanButton.setImageResource(R.drawable.metatyp_human_bw);
-        dwarfButton.setImageResource(R.drawable.metatyp_dwarf);
-        orcButton.setImageResource(R.drawable.metatyp_orc_bw);
-        trollButton.setImageResource(R.drawable.metatyp_troll_bw);
+        chosenMetatyp = "dwarf";
+        choseMetertyp("dwarf");
     }
 
     public void changeOrc(View v){
-        ImageButton elfButton = (ImageButton) findViewById(R.id.ElfButton);
-        ImageButton humanButton = (ImageButton) findViewById(R.id.HumanButton);
-        ImageButton dwarfButton = (ImageButton) findViewById(R.id.DwarfButton);
-        ImageButton orcButton = (ImageButton) findViewById(R.id.OrcButton);
-        ImageButton trollButton = (ImageButton) findViewById(R.id.TrollButton);
-
-        elfButton.setImageResource(R.drawable.metatyp_elf_bw);
-        humanButton.setImageResource(R.drawable.metatyp_human_bw);
-        dwarfButton.setImageResource(R.drawable.metatyp_dwarf_bw);
-        orcButton.setImageResource(R.drawable.metatyp_orc);
-        trollButton.setImageResource(R.drawable.metatyp_troll_bw);
+        chosenMetatyp = "orc";
+        choseMetertyp("orc");
     }
 
     public void changeTroll(View v){
+        chosenMetatyp = "troll";
+        choseMetertyp("troll");
+    }
+
+    public void choseMetertyp(String metatyp){
         ImageButton elfButton = (ImageButton) findViewById(R.id.ElfButton);
         ImageButton humanButton = (ImageButton) findViewById(R.id.HumanButton);
         ImageButton dwarfButton = (ImageButton) findViewById(R.id.DwarfButton);
         ImageButton orcButton = (ImageButton) findViewById(R.id.OrcButton);
         ImageButton trollButton = (ImageButton) findViewById(R.id.TrollButton);
 
-        elfButton.setImageResource(R.drawable.metatyp_elf_bw);
-        humanButton.setImageResource(R.drawable.metatyp_human_bw);
-        dwarfButton.setImageResource(R.drawable.metatyp_dwarf_bw);
-        orcButton.setImageResource(R.drawable.metatyp_orc_bw);
-        trollButton.setImageResource(R.drawable.metatyp_troll);
-    }
-
-    public void chngeSizw(View v){
-        ImageButton elfButton = (ImageButton) findViewById(R.id.ElfButton);
-        ImageButton humanButton = (ImageButton) findViewById(R.id.HumanButton);
-        ImageButton dwarfButton = (ImageButton) findViewById(R.id.DwarfButton);
-        ImageButton orcButton = (ImageButton) findViewById(R.id.OrcButton);
-        ImageButton trollButton = (ImageButton) findViewById(R.id.TrollButton);
-    }
-
-    private Bitmap resizeImage( final Bitmap image) {
-        Bitmap resizedImage = null;
-        if(image != null)
-        {
-            int maxHeight = 80; //actual image height coming from internet
-            int maxWidth = 150; //actual image width coming from internet
-
-            int imageHeight = image.getHeight();
-            if ( imageHeight > maxHeight )
-                imageHeight = maxHeight;
-            int imageWidth = (imageHeight*image.getWidth()) / image.getHeight();
-            if ( imageWidth > maxWidth ) {
-                imageWidth = maxWidth;
-                imageHeight = (imageWidth*image.getHeight()) / image.getWidth();
-            }
-            resizedImage = Bitmap.createScaledBitmap( image, imageWidth, imageHeight, true);
+        if(metatyp =="elf"){
+            elfButton.setImageResource(R.drawable.metatyp_elf);
+            humanButton.setImageResource(R.drawable.metatyp_human_bw);
+            dwarfButton.setImageResource(R.drawable.metatyp_dwarf_bw);
+            orcButton.setImageResource(R.drawable.metatyp_orc_bw);
+            trollButton.setImageResource(R.drawable.metatyp_troll_bw);
         }
-        return resizedImage;
+
+        if(metatyp =="human"){
+            elfButton.setImageResource(R.drawable.metatyp_elf_bw);
+            humanButton.setImageResource(R.drawable.metatyp_human);
+            dwarfButton.setImageResource(R.drawable.metatyp_dwarf_bw);
+            orcButton.setImageResource(R.drawable.metatyp_orc_bw);
+            trollButton.setImageResource(R.drawable.metatyp_troll_bw);
+        }
+
+        if(metatyp =="dwarf"){
+            elfButton.setImageResource(R.drawable.metatyp_elf_bw);
+            humanButton.setImageResource(R.drawable.metatyp_human_bw);
+            dwarfButton.setImageResource(R.drawable.metatyp_dwarf);
+            orcButton.setImageResource(R.drawable.metatyp_orc_bw);
+            trollButton.setImageResource(R.drawable.metatyp_troll_bw);
+        }
+
+        if(metatyp =="orc"){
+            elfButton.setImageResource(R.drawable.metatyp_elf_bw);
+            humanButton.setImageResource(R.drawable.metatyp_human_bw);
+            dwarfButton.setImageResource(R.drawable.metatyp_dwarf_bw);
+            orcButton.setImageResource(R.drawable.metatyp_orc);
+            trollButton.setImageResource(R.drawable.metatyp_troll_bw);
+        }
+
+        if(metatyp =="troll"){
+            elfButton.setImageResource(R.drawable.metatyp_elf_bw);
+            humanButton.setImageResource(R.drawable.metatyp_human_bw);
+            dwarfButton.setImageResource(R.drawable.metatyp_dwarf_bw);
+            orcButton.setImageResource(R.drawable.metatyp_orc_bw);
+            trollButton.setImageResource(R.drawable.metatyp_troll);
+        }
     }
+
+
+
+    public void startMetatypDetail(View v){
+        Intent startMetatypDetail = new Intent(this, MetatypDetail.class);
+        startMetatypDetail.putExtra("Metatyp",chosenMetatyp);
+        startActivity(startMetatypDetail);
+    }
+/*    public void startDaniel(View v){
+        Intent metaIntent = new Intent(this, Metatyp.class);
+        startActivity(metaIntent);
+    }*/
 }
