66bcfd23b724f6ca8028f841cb6fec5ae3a05e6e
oletei96 <teicho@gmx.de>
2017-07-11 11:40:01 +0200

    Java Classes Metatype and Skill

---
+++ app/src/main/java/com/example/danielojea/srapp/Classes/Skill.java
@@ -0,0 +1,31 @@
+package com.example.danielojea.srapp.Classes;
+
+/**
+ * Created by User on 11.07.2017.
+ */
+
+public class Skill {
+    private int Value;
+    private boolean Specialization;
+    private String SpecializationName;
+
+    public void setValue(int value) {
+        Value = value;
+    }
+
+    public boolean isSpecialization() {
+        return Specialization;
+    }
+
+    public void setSpecialization(boolean specialization) {
+        Specialization = specialization;
+    }
+
+    public String getSpecializationName() {
+        return SpecializationName;
+    }
+
+    public void setSpecializationName(String specializationName) {
+        SpecializationName = specializationName;
+    }
+}
