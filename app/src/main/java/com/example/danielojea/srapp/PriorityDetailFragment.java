26de01a0100f4a345ddd64708e902c3a4d1570a4
Ojeaschi <Ojeaschi@gmail.com>
2017-07-12 17:33:10 +0200

    anpassungen und aufräumen

--- app/src/main/java/com/example/danielojea/srapp/PriorityDetailFragment.java
+++ app/src/main/java/com/example/danielojea/srapp/PriorityDetailFragment.java
@@ -55,7 +55,11 @@ public class PriorityDetailFragment extends Fragment {
 
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.priorityitem_detail)).setText(mItem.details);
+            ((TextView) rootView.findViewById(R.id.priorityitem_detailA)).setText(mItem.details[0]);
+            ((TextView) rootView.findViewById(R.id.priorityitem_detailB)).setText(mItem.details[1]);
+            ((TextView) rootView.findViewById(R.id.priorityitem_detailC)).setText(mItem.details[2]);
+            ((TextView) rootView.findViewById(R.id.priorityitem_detailD)).setText(mItem.details[3]);
+            ((TextView) rootView.findViewById(R.id.priorityitem_detailE)).setText(mItem.details[4]);
         }
 
         return rootView;
--- app/src/main/java/com/example/danielojea/srapp/control/PriorityContentProvider.java
+++ app/src/main/java/com/example/danielojea/srapp/control/PriorityContentProvider.java
@@ -1,5 +1,9 @@
 package com.example.danielojea.srapp.control;
 
+import android.renderscript.RenderScript;
+
+import com.example.danielojea.srapp.Classes.PriorityRessource;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,40 +25,44 @@ public class PriorityContentProvider {
     public static final Map<String, PriorityContentProvider.PriorityItem> ITEM_MAP = new HashMap<String, PriorityContentProvider.PriorityItem>();
 
     static {
-        addItem(new PriorityContentProvider.PriorityItem("A", "Metatyp" , "A: \n" +
+        addItem(new PriorityContentProvider.PriorityItem("A", "Metatyp" ,new String[] {
                 "Mensch (9)\n" +
                 "Elf (8)\n" +
                 "Ork (7)\n" +
                 "Zwerg (7)\n" +
-                "Troll (5)\n\n" +
-                "B:\n" +
+                "Troll (5)"
+                ,
                 " Mensch (7)\n" +
                 "Elf (6)\n" +
                 "Ork (4)\n" +
                 "Zwerg (4)\n" +
-                "Troll (0)\n\n" +
-                "C:\n" +
+                "Troll (0)"
+                ,
                 "Mensch (5)\n" +
                 "Elf (3)\n" +
                 "Zwerg (1)\n" +
-                "Ork (0)\n\n" +
-                "D:\n" +
+                "Ork (0)"
+                ,
                 "Mensch (3)\n" +
-                "Elf (0)\n\n" +
-                "E:\n Mensch (1)"));
-        addItem(new PriorityContentProvider.PriorityItem("B", "Attribute" , "A: \n" +
-                "24\n\n" +
-                "B:\n" +
-                "20\n\n" +
-                "C:\n" +
-                "16\n\n" +
-                "D:\n" +
-                "14\n\n" +
-                "E:\n" +
-                "12"));
-        addItem(new PriorityContentProvider.PriorityItem("C", "Magie" , "Krasse Magie"));
-        addItem(new PriorityContentProvider.PriorityItem("D", "Fertigkeiten" , "Krasse Fertigkeiten"));
-        addItem(new PriorityContentProvider.PriorityItem("E", "Ressourcen" , "Krasser Reichtum"));
+                "Elf (0)"
+                ,
+                "Mensch (1)"}));
+        addItem(new PriorityContentProvider.PriorityItem("B", "Attribute" , new String[] {
+                "24",
+                "20",
+                "16",
+                "14",
+                "12"}));
+        addItem(new PriorityContentProvider.PriorityItem("C", "Magie" ,new String[] {"Krasse Magie","1 nice Magie","ok, du kannst Zaubern","Taschenspieler Tricks","verpiss dich"}));
+        addItem(new PriorityContentProvider.PriorityItem("D", "Fertigkeiten" , new String[] {"Krasse Fertigkeiten","du kannst schon was","Ok das ging schief ist aber nicht deine Schuld","Einfach Untalentiert","Schven"}));
+        //addItem(new PriorityContentProvider.PriorityItem("E", "Ressourcen" ,new String[] {"Krasser Reichtum","Du hast was gespart","Du klaust bei der Tafel","Du hast zwei Hypoteken auf deinem Wellblech Haus","Vögel bewerfen dich mit Brot"}));
+        PriorityRessource fillRessource = new PriorityRessource();
+        addItem(new PriorityContentProvider.PriorityItem("E", "Ressourcen", new String[] {
+                "Krasser Reichtum von "+fillRessource.getA()+"\t¥",
+                "Du hast "+fillRessource.getB()+"¥ gespart",
+                "Du klaust bei der Tafel \n"+fillRessource.getC()+"¥",
+                "Du hast zwei Hypoteken auf deinem Wellblech Haus und startest mit "+fillRessource.getD()+"¥",
+                "Vögel bewerfen dich mit Brot dabei findest du "+fillRessource.getE()+"¥"}));
     }
 
 
@@ -69,9 +77,9 @@ public class PriorityContentProvider {
     public static class PriorityItem {
         public final String id;
         public final String content;
-        public final String details;
+        public final String[] details;
 
-        public PriorityItem(String id, String content, String details) {
+        public PriorityItem(String id, String content, String[] details) {
             this.id = id;
             this.content = content;
             this.details = details;
