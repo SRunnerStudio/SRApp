b6f4d6631253443fefbac5178d06aa3d15e7e98b
DaSch93 <d.schoenberger@ymail.com>
2017-07-13 14:56:40 +0200

    Fonts+Profilbild ImageView

--- app/src/main/java/com/example/danielojea/srapp/charactercreation/CharacterCreation.java
+++ app/src/main/java/com/example/danielojea/srapp/charactercreation/CharacterCreation.java
@@ -2,6 +2,7 @@ package com.example.danielojea.srapp.charactercreation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.danielojea.srapp.R;
 
