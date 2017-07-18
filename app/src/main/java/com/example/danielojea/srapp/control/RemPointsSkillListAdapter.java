389dc5f51cdeda5f1ddacafd9b28fe2607d061c8
oletei96 <teicho@gmx.de>
2017-07-18 08:50:13 +0200

    Bug fix

--- app/src/main/java/com/example/danielojea/srapp/control/RemPointsSkillListAdapter.java
+++ app/src/main/java/com/example/danielojea/srapp/control/RemPointsSkillListAdapter.java
@@ -130,7 +130,7 @@ public class RemPointsSkillListAdapter extends RecyclerView.Adapter<RemPointsSki
                         notifyDataSetChanged();
                     }
                 } else {
-                    if ((character.getSkillPoints() > ((skill.getValue()+1)*2)) && skill.getValue() < 6) {
+                    if ((character.getKarma() > ((skill.getValue()+1)*2)) && skill.getValue() < 6) {
                         skill.setValue(skill.getValue() + 1);
                         character.setKarma(character.getKarma()-(skill.getValue()*2));
                         skillPointCounter.setText("Karmapunkte: " + character.getKarma());
